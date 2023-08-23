286a4ba6b4e571dee367ffe7c591594be957445a
ArunRathnasami <124259251+ArunRathnasami@users.noreply.github.com>
2023-08-23 20:57:31 +0100

    Update sketch_LCDNode_copy_20230815185327.ino

--- sketch_LCDNode_copy_20230815185327/sketch_LCDNode_copy_20230815185327.ino
+++ sketch_LCDNode_copy_20230815185327/sketch_LCDNode_copy_20230815185327.ino
@@ -1,46 +1,47 @@
-#include <ArduinoJson.h>
-#include <ESP8266WiFi.h>
+#include <ArduinoIoTCloud.h>
+#include <Arduino_ConnectionHandler.h>
+#include <WiFi.h>
+
+const char THING_ID[] = "b5666412-b34b-4504-b724-07381a397daa";
+
+float temperature;
+float humidity;
+float current;
+float distance;
 
 const char* ssid = "Batmobile";
 const char* password = "qwerty123";
 
 void setup() {
-  Serial.begin(9600);
-  WiFi.begin(ssid, password);
+  // Initialize Serial communication
+  Serial.begin(115200);
 
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
-
   Serial.println("Connected to WiFi");
+
+  // Initialize the ArduinoIoTCloud
+  initArduinoIoTCloud();
+
+  // Set up your connections
+  setConnectionHandler(&ArduinoIoTPreferredConnection);
+
+  // Begin the cloud connection
+  ArduinoCloud.begin(THING_ID);
 }
 
 void loop() {
-  if (Serial.available()) {
-    StaticJsonDocument<300> doc;
-    DeserializationError error = deserializeJson(doc, Serial);
+  ArduinoCloud.update();
 
-    if (!error) {
-      float humidity = doc["humidity"];
-      float temperature = doc["temperature"];
-      float current = doc["current"];
-      float distance = doc["distance"];
-
+  // Your code to read sensor data and other operations
   
-
-      Serial.print("Received Humidity: ");
-      Serial.println(humidity);
-      Serial.print("Received Temperature: ");
-      Serial.println(temperature);
-      Serial.print("Received Current: ");
-      Serial.println(current);
-      Serial.print("Received Distance: ");
-      Serial.println(distance);
-      Serial.println("-----------------------------------------");
-    } else {
-      Serial.println("Failed to parse data!");
-    }
-  }
+  // Update the cloud variables
+  ArduinoCloud.setFloat(temperature, "temperature");
+  ArduinoCloud.setFloat(humidity, "humidity");
+  ArduinoCloud.setFloat(current, "current");
+  ArduinoCloud.setFloat(distance, "distance");
 }
-
