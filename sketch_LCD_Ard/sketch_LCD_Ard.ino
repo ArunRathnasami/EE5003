56f770f2b73dc9458282ee405a3e233d01b19388
ArunRathnasami <124259251+ArunRathnasami@users.noreply.github.com>
2023-08-23 20:15:26 +0100

    Update sketch_LCD_Ard.ino
    Forget to upload the code for Node MCU

--- sketch_LCD_Ard/sketch_LCD_Ard.ino
+++ sketch_LCD_Ard/sketch_LCD_Ard.ino
@@ -1,4 +1,4 @@
-#include "DHT.h"
+#include "ArduinoIoTCloud.h"
 #include <ArduinoJson.h>
 #include <LiquidCrystal.h>
 
@@ -11,6 +11,11 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 DHT dht(DHTPIN, DHTTYPE);
 
+float temperature;
+float humidity;
+float current;
+float distance;
+
 void setup() {
   Serial.begin(9600);
   dht.begin();
@@ -18,38 +23,37 @@ void setup() {
   pinMode(ECHO_PIN, INPUT);
   lcd.begin(16, 2);
   delay(2000);
+
+  ArduinoCloud.setThingId("your_thing_id");
+  ArduinoCloud.addProperty(temperature, READ, "Temperature", "C");
+  ArduinoCloud.addProperty(humidity, READ, "Humidity", "%");
+  ArduinoCloud.addProperty(current, READ, "Current", "A");
+  ArduinoCloud.addProperty(distance, READ, "Distance", "cm");
+
+  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
 }
 
 void loop() {
   float h = dht.readHumidity();
   float t = dht.readTemperature();
-  float current = readACS712();
-  float distance = readUltrasonicDistance();
+  current = readACS712();
+  distance = readUltrasonicDistance();
 
   // Display data on the LCD
   lcd.setCursor(0, 0);
   lcd.print("Temp: ");
   lcd.print(t);
   lcd.print("C");
-  
+
   lcd.setCursor(0, 1);
   lcd.print("Dist: ");
   lcd.print(distance);
   lcd.print("cm");
-  
-  // Create a JSON document to hold our sensor readings
-  StaticJsonDocument<300> doc;
-  doc["humidity"] = h;
-  doc["temperature"] = t;
-  doc["current"] = current;
-  doc["distance"] = distance;
 
-  // Serialize JSON and print to serial
-  serializeJson(doc, Serial);
-  Serial.println();
-
+  ArduinoCloud.update();
   delay(1000);
 }
+
 
 float readACS712() {
   int raw = analogRead(ACS_PIN);
@@ -72,3 +76,5 @@ float readUltrasonicDistance() {
   float distance = duration * 0.034 / 2;
   return distance;
 }
+
+
